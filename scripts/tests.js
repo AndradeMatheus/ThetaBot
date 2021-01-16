15c5ca5c11ca6bfba509800a4c8f0a1e14a9f94e
AndradeMatheus <matheus.andrade.santos@gmail.com>
2021-01-16 13:26:57 -0300

    Implementado play dinâmico do my  instants

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,14 +1,47 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('../config');
+const token = config.configs.auth.token;
+const prefix = config.configs.config.prefix;
+const botId = '799778892780011530'
+const axios = require('axios');
+const ytdl = require('ytdl-core');
 
-client.on('ready', () => {
-  console.log(`Logged in as TetaBotnp,!`);
+client.once('ready', () => {
+  console.log(`EAE MACACO!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', async msg => {
+  if(msg.author.id != botId && msg.channel.name == 'theta-bot'){
+    if(msg.content.startsWith(`${prefix}agu`)){
+      msg.channel.send(new Discord.MessageAttachment(`https://cdn.discordapp.com/attachments/785238813919805451/785707669125464104/unknown.png`))
+    }
+    else if(msg.content.startsWith(`${prefix}macaco-instants`)){
+      let search = msg.content.replace(`${prefix}macaco-instants `, "");
+      let instant = await getMyInstants(search);
+      if (msg.member.voice.channel) {
+        const connection = await msg.member.voice.channel.join();
+        connection.play(instant.sound);
+      }
+    }
+    else{
+      msg.channel.send("eu nao to funcionando direito nao seu animal")
+    }
   }
+  console.log(`${msg.content} do autor ${msg.author.name}`)
 });
 
-client.login('token');
+async function getMyInstants(search) {
+  let query = search.replace(/ /g, "-");
+  try{
+    const response = await axios.get(`https://www.myinstants.com/api/v1/instants/${query}`)
+    if(response.data.sound){
+      return response.data;
+    }
+  }
+  catch(err){
+    return err;
+  }
+}
+
+client.login(token);
