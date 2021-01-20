05b9597c7b7cf39268b19d47a4002c68dd0bcd27
AndradeMatheus <matheus.andrade.santos@gmail.com>
2021-01-19 21:11:09 -0300

    Adicionado criação de alias para um som

--- scripts/commands.js
+++ scripts/commands.js
@@ -2,21 +2,22 @@ const Discord = require('discord.js');
 const axios = require('axios');
 const config = require('../config');
 const Command = require('./command');
+const fs = require('fs');
 const { prefix, botId } = config.config;
 
 const handleAgu = async (msg) => {
     msg.channel.send(new Discord.MessageAttachment(`https://cdn.discordapp.com/attachments/785238813919805451/785707669125464104/unknown.png`));
 }
 
-const handleInstant = async (msg) => {
-    let search = msg.content.replace(`${prefix}inst `, '');
+const handleInstant = async (msg, command = null) => {
+    let search = !command ? msg.content.replace(`${prefix}inst `, '') : command;
     let instant = await getMyInstants(search);
   
     if (!!instant.sound && msg.member.voice.channel) {
         const connection = await msg.member.voice.channel.join();
         connection.play(instant.sound);
 
-        setTimeout(_ => msg.member.voice.channel.leave(), 15000);
+        setTimeout(_ => msg.member.voice.channel.leave(), 300000);
     }
     else {
         msg.reply("instant não encontrado");
@@ -34,14 +35,97 @@ const getMyInstants = async(search) => {
     }
 }
 
+const getInstantAlias = async(command, msg) => {
+    fs.readFile('./storage/instants-aliases.json', (err, data) => {
+        if (err) {
+            console.log("ocorreu um erro ao ler o arquivo de aliases.");
+            msg.reply("ocorreu um erro ao ler o arquivo de aliases ou ele não existe.");
+        }else{
+            command = command.replace(prefix, '');
+            const file =  JSON.parse(data.toString());
+            let server = file.servers[msg.guild.id];
+            
+            if(server){
+                handleInstant(msg, server.aliases[command])
+            }
+            else msg.reply("Não existem aliases nesse servidor.");
+        }
+    });
+}
+
+const handleInstantCreateAlias = async(msg) => {
+    if(!fs.existsSync('./storage/instants-aliases.json')){
+        data = JSON.stringify({servers: {}});
+        fs.writeFileSync('./storage/instants-aliases.json', data);
+    }
+
+    try{
+        let command = msg.content.replace(`${prefix}inst-create `, '');
+        let alias = command.split(' ')[0];
+        let sound = command.split(' ')[1].includes('myinstants.com') ? command
+                                                                    .split(' ')[1]
+                                                                    .slice(command.split(' ')[1].lastIndexOf("instants/")+9, 
+                                                                    command.split(' ')[1].length).replace("/", "") 
+                                                                    : command.split(' ')[1].replace("/", "");
+
+        fs.readFile('./storage/instants-aliases.json', (err, data) => {
+            if (err) {
+                throw err;
+            }
+            const file = JSON.parse(data.toString());
+            let server = file.servers[msg.guild.id];
+
+            if(server) server.aliases[alias] = sound;
+            else file.servers[msg.guild.id] = {aliases: {[alias]: sound}};
+
+            fs.writeFileSync('./storage/instants-aliases.json', JSON.stringify(file));
+        });
+
+        msg.reply(`seu alias **${alias}** do som **${sound}** foi criado com sucesso!`)
+    }
+    catch(err){
+        msg.reply("Não foi possível criar o alias, verifique o seu comando.")
+    }
+}
+
+const handleInstantListAlias = async (msg) => {
+    fs.readFile('./storage/instants-aliases.json', (err, data) => {
+        if (err) {
+            console.log("ocorreu um erro ao ler o arquivo de aliases.");
+            msg.reply("ocorreu um erro ao ler o arquivo de aliases ou ele não existe.");
+        }else{
+            const file =  JSON.parse(data.toString());
+            let server = file.servers[msg.guild.id];
+            
+            if(server){
+                let aliases = []
+                
+                Object.entries(server.aliases).map(([k, v]) => aliases.push({name:`${prefix}${k}`, value:v}))
+                
+                const embed = new Discord.MessageEmbed()
+                .setTitle('Lista de alias')
+                .setColor(0x5b34eb)
+                .addFields(aliases)
+                .setThumbnail('https://i.gifer.com/iXR.gif')
+                
+                msg.channel.send(embed);
+            }
+            else msg.reply("Não existem aliases nesse servidor.");
+        }
+    });
+}
+
+const handleInstantDeleteAlias = async (msg) => {
+    msg.reply("esse comando não funciona ainda, não saber ler, macaco?")
+}
+
+const handleInstantEditAlias = async (msg) => {
+    msg.reply("esse comando não funciona ainda, não saber ler, macaco?")
+}
+
 const handleDilera = async (msg) => {
   const connection = await msg.member.voice.channel.join();
   connection.play("https://www.myinstants.com/media/sounds/buzinaprolongada.mp3");
-}
-
-const handleTey = async (msg) => {
-  const connection = await msg.member.voice.channel.join();
-  connection.play("https://www.myinstants.com/media/sounds/tey-quietinho-bydilera.mp3");
 }
 
 const handleHelp = async (msg) => {
@@ -86,13 +170,16 @@ const handleStop = async (msg) => {
 const commands = [
     new Command(`${prefix}agu`, 'Exibe retrato verossímil de Lucão e Ninext', handleAgu),
     new Command(`${prefix}inst`, 'Busca áudio no MyInstants', handleInstant),
+    new Command(`${prefix}inst-create`, 'Define um alias pra uma url do MyInstants', handleInstantCreateAlias),
+    new Command(`${prefix}inst-list`, 'Lista os aliases criados nesse servidor', handleInstantListAlias),
+    new Command(`${prefix}inst-edit`, 'Não implementado', handleInstantDeleteAlias),
+    new Command(`${prefix}inst-delete`, 'Não implementado', handleInstantEditAlias),
     new Command(`${prefix}dilera`, 'Buzina dilera', handleDilera),
-    new Command(`${prefix}tey`, 'Tey! Quetinho!', handleTey),
     new Command(`${prefix}leave`, 'Remove o bot do canal de voz :(', handleLeave),
-    new Command(`${prefix}pause`, 'Pausa o aúdio', handlePause),
-    new Command(`${prefix}resume`, 'Resume o áudio', handleResume),
-    new Command(`${prefix}stop`, 'Para o aúdio', handleStop),
+    new Command(`${prefix}pause`, 'Pausa a reprodução do aúdio atual', handlePause),
+    new Command(`${prefix}resume`, 'Resume o áudio pausado', handleResume),
+    new Command(`${prefix}stop`, 'Cancela a reprodução do áudio', handleStop),
     new Command(`${prefix}help`, 'Help!', handleHelp)
 ]
 
-module.exports = commands;
+module.exports = {commands, getInstantAlias};
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const { config, auth } = require('../config');
 const client = new Discord.Client();
-const commands = require('./commands');
+const {commands, getInstantAlias} = require('./commands');
 
 client.on('message', async msg => {
   if(msg.author.id != config.botId && msg.channel.name == 'theta-bot'){
@@ -10,13 +10,14 @@ client.on('message', async msg => {
         const cmd = commands.find(c => c.name === commandName);
 
         if (cmd) await cmd.execute(msg);
+        else getInstantAlias(commandName, msg);
     }
-    
+
     if(msg.content.includes("bot") && msg.content.includes("funcionand")){
       msg.reply("eu nao to funcionando direito nao seu animal");
     }
 
-    console.log(`${msg.content} do autor ${msg.author.name}`);
+    console.log(`${msg.content} do autor ${msg.author.username}`);
   }  
 });
 
