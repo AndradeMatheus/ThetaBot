0e14bc47738cd4f703cf23cebde85b78690ec78f
AndradeMatheus <matheus.andrade.santos@gmail.com>
2021-01-23 23:54:03 -0300

    Rework no card de música

--- scripts/assets.js
+++ scripts/assets.js
@@ -2,5 +2,20 @@ module.exports = {
     lucasNinext: 'https://cdn.discordapp.com/attachments/785238813919805451/785707669125464104/unknown.png',
     dileraBuzina: 'https://www.myinstants.com/media/sounds/buzinaprolongada.mp3',
     gatoPop: 'https://i.kym-cdn.com/photos/images/newsfeed/001/931/959/2e4.gif',
-    macacoNotebook: 'https://i.gifer.com/iXR.gif'
+    macacoNotebook: 'https://i.gifer.com/iXR.gif',
+    musicPlayer:
+        {
+            paused: 'https://i.imgur.com/obsdDGV.png',
+            playing: 'https://i.imgur.com/oZTKIKD.png'
+        },
+    youtube: 
+        {
+            logo: 'https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-6-2.png',
+            color: '#FF0000'
+        },
+    spotify:
+        {
+            logo: 'https://www.benlcollins.com/wp-content/uploads/2016/01/spotify.png',
+            color: '#1DB954'
+        }
 }
--- scripts/music.commands.js
+++ scripts/music.commands.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
 const { promisify } = require('util');
-const readFile = promisify(fs.readFile);   
 const Discord = require('discord.js');
 const assets = require('./assets')
 const axios = require('axios');
@@ -9,13 +7,18 @@ const { BOT_PREFIX: prefix } = process.env;
 
 const handleMusic = async (msg)=> {
     const embed = new Discord.MessageEmbed()
-    .setTitle('Help')
-    .setAuthor('YouTube', 'https://www.pinclipart.com/picdir/middle/530-5305994_icon-youtube-logo-png-clipart.png', 'http://Youtube.com')
-    .setColor(0x5b34eb)
+    .setAuthor('YouTube', assets.youtube.logo, 'http://youtube.com')
+    .setColor(assets.youtube.color)
     .setTitle("Barões da Pisadinha")
     .setDescription("Recairei")
     .setThumbnail('https://studiosol-a.akamaihd.net/tb/652x652/palcomp3-discografia/7/2/f/2/78abc8f4a92e421582ab8de53833b5f2.jpg')
-    .setFooter('Playing on call de doente', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThwie-fFbiGCuFgRxD4nvn1uJzi7bddy_R8g&usqp=CAU');
+    .addFields(
+        { name: '\u200B', value: '\u200B' },
+		{ name: 'Pedido por:⠀⠀⠀⠀⠀', value: msg.author.username , inline: true },
+        { name: 'Duração:⠀⠀⠀⠀⠀⠀', value: '13:25m', inline: true },
+		{ name: 'Próxima:', value: 'nenhuma', inline: true },
+	)
+    .setFooter('Playing on TEST', assets.musicPlayer.playing);
 
     msg.channel.send({embed: embed}).then(message => {
         message.react("⏮️")
