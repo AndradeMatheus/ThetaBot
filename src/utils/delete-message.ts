4ef900cbdc89b5b4297fdfb40d1128e9ab8be2e2
Lucas Souza <lucasvsouza28@gmail.com>
2022-05-17 14:31:31 -0300

    chore: removing unused files

--- src/utils/delete-message.ts
+++
@@ -1,7 +0,0 @@
-import { Message } from 'discord.js';
-
-export const handleDeleteMessage = async (msg: Message, timeout: number) => {
-  setTimeout(function() {
-    msg.delete();
-  }, timeout);
-};
