ca26409137b2ee7c9913a95373b3cfe0ad5dc2e4
Lucas Souza <lucasvsouza28@gmail.com>
2022-05-22 14:06:41 -0300

    refactor: organizing index file

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,25 @@ import 'reflect-metadata';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { Client } from 'discord.js';
+import { container } from 'tsyringe';
 import IEnvironment from 'interfaces/environment';
 import initDatabase from './utils/database';
 import loadDIContainer, { Tokens } from './utils/loadContainer';
-import { Client } from 'discord.js';
+import logger from './utils/logger';
 import ISlashCommandsService from './interfaces/services/slash-commands';
 import MyInstantsSlashCommand from './slash-commands/inst';
-import { container } from 'tsyringe';
-import logger from './utils/logger';
 loadDIContainer();
 
 const environment = container.resolve<IEnvironment>(
   Tokens.IEnvironment,
 );
-
-initDatabase(environment);
-
 const slashCommandService = container.resolve<ISlashCommandsService>(
   Tokens.ISlashCommandsService,
 );
+
+initDatabase(environment);
+
 slashCommandService.loadCommands();
 
 const client = new Client({
@@ -29,6 +29,15 @@ const client = new Client({
 });
 
 client.login(environment.Token);
+
+client.once('ready', () => {
+  client?.user?.setActivity('.help', {
+    type: 'STREAMING',
+    url: 'http://twitch.tv/tetistiger',
+  });
+
+  logger.info('EAE MACACO!');
+});
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
@@ -43,13 +52,4 @@ client.on('interactionCreate', async (interaction) => {
     );
     myInstantsSlashCommand.handleCustomCommand(interaction);
   }
-});
-
-client.once('ready', () => {
-  client?.user?.setActivity('.help', {
-    type: 'STREAMING',
-    url: 'http://twitch.tv/tetistiger',
-  });
-
-  logger.info('EAE MACACO!');
 });
